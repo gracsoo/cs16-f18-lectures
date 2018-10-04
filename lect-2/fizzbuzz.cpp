78ac5cf6bdcd2cbdd5b76bb9805147b0b70b6b37
Diba Mirza <dimirza@cs.ucsb.edu>
2018-10-04 13:00:59 -0700

    Update fizzbuzz.cpp

--- lect-2/fizzbuzz.cpp
+++ lect-2/fizzbuzz.cpp
@@ -1,24 +1,9 @@
+//fizzbuzz.cpp
 #include <iostream>
-// This is  version 2
-// include a library for input output
+
 using namespace std;
 //allows referring to specific modules in the standard namespace
-
-int foobar(){
-    return 5;
-}
 int main(){
-    int x; //Declare the variable x
-    x = 3; //Initialize x
-
-    int y =10;
-    int z;
-
-    string name;
-    name = "Diba";
-
-    //Execution always starts with main()
-    cout<<"Hello CS16! "<<name<<endl;
-    foobar();
+    
     return 0;
 }
